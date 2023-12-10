d82ef345df454231287b4f902881639a441c639a
Perhat Amanlyyev <116658502+PEPEZHK@users.noreply.github.com>
2023-12-10 16:02:10 +0300

    Delete IdeaProjects directory

--- IdeaProjects/demo/src/main/java/com/example/demo/Firebase/controller.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Firebase;
-
-public class controller {
-    private static FireBaseService serv ;
-
-    public static void main(String[] args) {
-        FirebaseDataWriter w = new FirebaseDataWriter() ;
-    }
-}
